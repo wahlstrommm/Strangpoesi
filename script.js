0a80fea6de1a7788f0ed4418bf9e5ab4bc4580cb
Magnus Vahlström <Wahlstrommm@gmail.com>
2021-11-30 15:41:29 +0100

    added some code

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ add()
 function add(){
     
     for(i=0;i<list.length; i++){
-        var div1 = document.getElementById("div1").innerHTML += "<button>" + list[i] + "</button>";
-        var btn = document.createElement("button");
+        //var div1 = document.getElementById("div1").innerHTML += "<button>" + list[i] + "</button>";
+        var btn = document.createElement("button").innerHTML +=  list[i].value;
+        console.log(btn.value);
     }
 }
 
