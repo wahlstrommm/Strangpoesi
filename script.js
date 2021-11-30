8d90569039e52da1f8b9ba4327d871bd1d42a94a
Magnus Vahlström <Wahlstrommm@gmail.com>
2021-11-30 12:53:33 +0100

    added script.js and index.html

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+let root = document.getElementById("root");
+
+
+
+
+
+list =["går","hälsade","jag","visste","om","senare","alltid","när","stan","ko","såg","i","sällan","fes","ramla"];
+
+add()
+
+function add(){
+    
+    for(i=0;i<list.length; i++){
+        var div1 = document.getElementById("div1").innerHTML += "<button>" + list[i] + "</button>";
+        var btn = document.createElement("button");
+    }
+}
+
