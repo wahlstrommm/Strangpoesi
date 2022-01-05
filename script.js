74e2811e95baf9de21ff6733655cf26979b747db
Magnus Vahlström <Wahlstrommm@gmail.com>
2022-01-05 20:31:18 +0100

    added some code

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-
-
-
 let root = document.getElementById("root");
+let btnPrint = document.getElementById("btnPrint");
+printList = [];
+let text = document.createElement("p");
+list = ["går", "hälsade", "jag", "visste", "om", "senare", "alltid", "när", "stan", "ko", "såg", "i", "sällan", "fes", "ramla"];
 
+add();
 
-
-
-
-list =["går","hälsade","jag","visste","om","senare","alltid","när","stan","ko","såg","i","sällan","fes","ramla"];
-
-add()
-
-function add(){
-    
-    for(i=0;i<list.length; i++){
-        //var div1 = document.getElementById("div1").innerHTML += "<button>" + list[i] + "</button>";
-        var btn = document.createElement("button").innerHTML +=  list[i].value;
-        console.log(btn.value);
-    }
+function add() {
+  for (i = 0; i < list.length; i++) {
+    //var div1 = document.getElementById("div1").innerHTML += "<button>" + list[i] + "</button>";
+    let btnlet = document.createElement("button");
+    btnlet.id = list[i];
+    btnlet.innerText = list[i];
+    root.append(btnlet);
+    btnlet.addEventListener("click", function () {
+      printList.push(btnlet.innerText);
+      console.log(printList);
+    });
+  }
 }
-
+btnPrint.addEventListener("click", function () {
+  text.innerText = printList.join(" ");
+  root.append(text);
+});
